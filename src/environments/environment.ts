bfea69b21dc865dcdd395774044ffa36233f44be
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-08-26 01:34:14 -0300

    CSS é incrível e confuso

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCWV7nIrHcpLymNmgEhJyKIeuYN8yYBUTo",
+    authDomain: "requisicoes-app-cc23b.firebaseapp.com",
+    projectId: "requisicoes-app-cc23b",
+    storageBucket: "requisicoes-app-cc23b.appspot.com",
+    messagingSenderId: "322602786410",
+    appId: "1:322602786410:web:7d09cd4eeed2a9b62ef413"
+  }
 };
 
 /*
