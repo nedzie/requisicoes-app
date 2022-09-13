f8718c305c4ceca0aa92466d8c2c7109ff4a26c2
Marcos Fernandes <marcosfernandes@uniplaclages.edu.br>
2022-09-13 15:54:30 -0300

    Att environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCWV7nIrHcpLymNmgEhJyKIeuYN8yYBUTo",
+    authDomain: "requisicoes-app-cc23b.firebaseapp.com",
+    projectId: "requisicoes-app-cc23b",
+    storageBucket: "requisicoes-app-cc23b.appspot.com",
+    messagingSenderId: "322602786410",
+    appId: "1:322602786410:web:7d09cd4eeed2a9b62ef413"
+  }
 };
